1a03e968fe2393914463aae9154495b9e694a9d1
Jules <julz.tahal@gmail.com>
2024-06-16 19:28:46 -0400

    commit

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-let name = "Jules"; //String Litersl
-let age = 30; //Number Literal
-let isApproved = true; // Boolean Literal
-let firstName; // Undefined or let firstName = undefined;
-let lastName = null;
+function square(number) {
+  return number * number;
+}
 
-let selectedColor = ["red", "blue"];
-console.log(selectedColor[0]);
-
-selectedColor[2] = "green";
-console.log(selectedColor);
+console.log(square(2));
