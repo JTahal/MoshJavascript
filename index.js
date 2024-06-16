1e1b0435ed94dc7fab086f509dae7a210d538a33
Jules <julz.tahal@gmail.com>
2024-06-16 18:50:48 -0400

    commit

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let age = 30; //Number Literal
 let isApproved = true; // Boolean Literal
 let firstName; // Undefined or let firstName = undefined;
 let lastName = null;
+
+let selectedColor = ["red", "blue"];
+console.log(selectedColor[0]);
+
+selectedColor[2] = "green";
+console.log(selectedColor);
